5275e48f283255b7fc7f836aaef2aed7f39be524
eastmalon <pzd731@gmail.com>
2017-01-19 07:10:32 +0000

    update

--- genpac2.sh
+++ genpac2.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cp ./whitelist.pac ./whitelist_2.pac && sed -i "s/127.0.0.1:1080/127.0.0.1:1088/g" ./whitelist_2.pac
+cp ./whitelist.pac ./whitelist_2.pac && sed -i "s/127.0.0.1:1080/127.0.0.1:1084/g" ./whitelist_2.pac
